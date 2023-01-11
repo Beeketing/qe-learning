5b75a3f5a5c16603c7e185bb7426cff08caa234c
Linh Phan <linhphan@beeketing.net>
2023-01-11 14:17:49 +0700

    learn(typescript): add lesson 2

--- talent-class/linhphan/index.js
+++
@@ -1,4 +0,0 @@
-var add = function (a, b) { return (b ? a + b : a); };
-console.log(add(1, 2));
-console.log(add(3));
-var addNonNull = function (a, b) { return a + b; };
--- talent-class/linhphan/index.ts
+++
@@ -1,4 +0,0 @@
-const add = (a: number, b?: number) => (b? a + b : a)
-console.log(add(1,2),)
-console.log(add(3))
-const addNonNull = (a: number, b?: number) => a + b
