80bc1bd733d4360284453933318cc15af7ac934c
truongnxse05891 <93703575+truongnxse05891@users.noreply.github.com>
2023-01-12 03:06:42 +0700

    exercise(ts): 3

---
+++ talent-class/truongnguyen/exercise-ts-slide-06.ts
@@ -0,0 +1,45 @@
+test('has title shopbase', async ({ page }) => {
+    await page.goto('https://www.shopbase.com/');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/ShopBase/);
+  });
+  
+  test('add to cart', async ({ page }) => {
+    await page.goto('https://hiendo2.sbprod.tk/products/lee-straight-jean');
+    await page.waitForTimeout(5 *1000);
+  
+    await page.click("//button[@id ='add-to-cart']");
+    
+  
+    console.log("add successfully");
+    await page.waitForTimeout(5 *1000);
+  
+    const cartNumber = await page.locator("//span[@class='cart-number']").textContent();
+    console.log(cartNumber);
+    await expect(cartNumber).toEqual("01");
+  
+  });
+  
+  test('check cart', async ({ page }) => {
+    await page.goto('https://hiendo2.sbprod.tk/products/lee-straight-jean');
+    await page.waitForTimeout(3 *1000);
+  
+    await page.click("//button[@id ='add-to-cart']");
+    
+  
+    console.log("add successfully");
+    // await page.waitForTimeout(5 *1000);
+  
+    // const cartNumber = await page.locator("//span[@class='cart-number']").textContent();
+    // console.log(cartNumber);
+    await page.goto('https://hiendo2.sbprod.tk/cart');
+    await page.waitForTimeout(3 *1000);
+    const numberItems = await page.inputValue("//input[@type='number']");
+    console.log(numberItems);
+    const price = await page.locator("//p[@class='h5 product-cart__price']//span").textContent();
+    console.log(price);
+     expect(numberItems).toEqual("1");
+     expect(price).toEqual("$188.00");
+  
+  });
