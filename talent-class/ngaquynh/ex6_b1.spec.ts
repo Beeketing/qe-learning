59dbda89985053fa615ba24490d4e3fc791c76cc
Nga <ngaquynh141001@gmail.com>
2023-01-12 02:44:14 +0700

    learn(PlayWright): add bt1,2

---
+++ talent-class/ngaquynh/ex6_b1.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+
+
+
+test('addProduct', async ({ page }) => {
+  //admin login
+  await page.goto('https://accounts.shopbase.com/sign-in?return_url=https%3A%2F%2F16-clothing.onshopbase.com%2Fadmin%2F');
+  await page.waitForTimeout(2*1000);
+  // await page.pause()
+  await page.locator("//input[@id='email']").fill('tuyetle+1@beeketing.net');
+  await page.locator("//input[@id='password']").fill('123456');
+  await page.click("//button[@type='submit']")
+  await page.waitForTimeout(5*1000)
+
+  //add product
+  await page.click("//span[normalize-space()='Products']");
+  await page.waitForTimeout(1*1000);
+  await page.click("//span[normalize-space()='All products']");
+  await page.waitForTimeout(1*1000);
+  await page.click("//span[normalize-space()='Add product']");
+  await page.waitForTimeout(1*1000);
+  await page.locator("//input[@placeholder='Short Sleeve T-Shirt']").fill('iPhone 14 Pro Max 128GB - Nga');
+  await page.waitForTimeout(1*1000);
+  await page.locator("//input[@id='price']").fill('10000');
+  await page.waitForTimeout(1*1000);
+  await page.locator("//input[@id='compare_price']").fill('12000');
+  await page.waitForTimeout(1*1000);
+  await page.click("//a[@class='pull-right']");
+  await page.waitForTimeout(1*1000);
+  await page.locator("//input[@id='option-name']").fill('color');
+  await page.waitForTimeout(1*1000);
+  await page.locator("//input[@placeholder='Separate options with comma']").fill('Space black,Silver,Gold,Deep Purple,');
+  await page.waitForTimeout(1*1000);
+  await page.click("//button[@class='btn btn-primary']");
+  await page.waitForTimeout(5*1000);
+  
+  await page.goto('https://16-clothing.onshopbase.com/products/iphone-14-pro-max-128gb-nga-1');
+  await page.waitForTimeout(5*1000);
+  const product = await page.locator("//h1[@class='h4 d-block product__name mt0 mb12 product__name-product']").textContent()
+  //expects 
+  expect(product).toEqual('iPhone 14 Pro Max 128GB - Nga')
+});
+
+
